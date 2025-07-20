6e109933e08dc45e97a6935f57454cb6babcab25
akindeledby <akindeledby86@gmail.com>
2025-07-20 19:59:13 +0100

    Third commit

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -8,5 +8,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const userController_1 = require("../controllers/userController");
 const router = express_1.default.Router();
-router.post("/users", userController_1.registerUser);
+router.post("/users/register", userController_1.registerUser);
 exports.default = router;
--- dist/src/index.js
+++ dist/src/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
@@ -9,7 +11,7 @@ const walletRoutes_1 = __importDefault(require("./routes/walletRoutes"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
-    res.send(`
+  res.send(`
     <h1>Lendsqr Wallet Service</h1>
     <p>Available endpoints:</p>
     <ul>
@@ -20,10 +22,10 @@ app.get("/", (req, res) => {
     </ul>
   `);
 });
-app.use("/api/users", userRoutes_1.default);
+app.use("/api/users/register", userRoutes_1.default);
 app.use("/api/wallets", walletRoutes_1.default);
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
 exports.default = app;
 // import express from "express";
